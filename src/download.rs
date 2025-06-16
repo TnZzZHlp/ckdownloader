2d8f7841413a02309df2847a2a01d00e82d9c2ac
TnZzZHlp <tnzzzhlp@outlook.com>
2025-06-16 12:07:38 +0800

    feat: 添加 retrying 库以支持下载重试机制

--- src/download.rs
+++ src/download.rs
@@ -1,6 +1,7 @@
 use futures_util::StreamExt;
 use indicatif::ProgressStyle;
 use reqwest::header;
+use retrying::retry;
 use std::path::Path;
 use std::sync::Arc;
 use tokio::fs::{self, OpenOptions};
@@ -44,42 +45,58 @@ pub async fn download_attachments(
         let domain = Arc::clone(&domain);
 
         tasks.spawn(async move {
-            let _permit = SEM.acquire().await;
+            let att = att.clone();
+            let _ = download(att, &output, &username, &domain).await;
+            video_pbar.inc(1);
+        });
+    }
 
-            let folder = format!("{}/{}", output, username);
-            let _ = fs::create_dir_all(&folder).await;
-            let path = format!("{}/{}", folder, att.name);
-            let mut downloaded = 0u64;
-            if Path::new(&path).exists() {
-                downloaded = fs::metadata(&path).await.unwrap().len();
-            }
-            let mut req = CLIENT.get().unwrap().get(format!(
-                "{}/data{}",
-                att.server.as_ref().unwrap_or(&format!(
-                    "https://{}",
-                    domain
-                )),
-                att.path
-            ));
-            if downloaded > 0 {
-                req = req.header(header::RANGE, format!("bytes={}-", downloaded));
-            }
+    tasks.join_all().await;
 
+    Ok(())
+}
 
-            let resp = match req.send().await {
-                Ok(resp) => resp,
-                Err(err) => {
-                    let _ = PB.println(format!("下载失败: {} - {}", att.name, err));
-                    return;
-                }
-            };
+#[retry(stop=attempts(2))]
+async fn download(
+    att: Attachment,
+    output: &str,
+    username: &str,
+    domain: &str,
+) -> Result<(), anyhow::Error> {
+    let _permit = SEM.acquire().await;
 
-            let pb = PB.add(indicatif::ProgressBar::new(
-                downloaded + resp.content_length().unwrap_or(0),
-            ));
-            pb.set_message(format!("正在下载: {}", att.name));
-            pb.enable_steady_tick(std::time::Duration::from_millis(100));
-            pb.set_style(
+    let folder = format!("{}/{}", output, username);
+    let _ = fs::create_dir_all(&folder).await;
+    let path = format!("{}/{}", folder, att.name);
+    let mut downloaded = 0u64;
+    if Path::new(&path).exists() {
+        downloaded = fs::metadata(&path).await.unwrap().len();
+    }
+    let mut req = CLIENT.get().unwrap().get(format!(
+        "{}/data{}",
+        att.server
+            .as_ref()
+            .unwrap_or(&format!("https://{}", domain)),
+        att.path
+    ));
+    if downloaded > 0 {
+        req = req.header(header::RANGE, format!("bytes={}-", downloaded));
+    }
+
+    let resp = match req.send().await {
+        Ok(resp) => resp,
+        Err(err) => {
+            let _ = PB.println(format!("Download failed: {} - {}", att.name, err));
+            return Err(anyhow::anyhow!("Download failed: {} - {}", att.name, err));
+        }
+    };
+
+    let pb = PB.add(indicatif::ProgressBar::new(
+        downloaded + resp.content_length().unwrap_or(0),
+    ));
+    pb.set_message(format!("正在下载: {}", att.name));
+    pb.enable_steady_tick(std::time::Duration::from_millis(100));
+    pb.set_style(
                 ProgressStyle::with_template(
                     "[{elapsed_precise}] [{wide_bar:.magenta/blue}] [{decimal_bytes}/{decimal_total_bytes}] [{decimal_bytes_per_sec}] {eta}🔄"
                 )
@@ -87,68 +104,67 @@ pub async fn download_attachments(
                 .progress_chars("#>-"),
             );
 
-            if resp.status() == 416 {
-                let _ = PB.println(format!(
-                    "文件已下载完成: {} - {}",
-                    resp.status(),
-                    resp.url()
-                ));
-                video_pbar.inc(1);
-                pb.finish_and_clear();
-                return;
-            }
-            if !(resp.status().is_success() || resp.status() == 206) {
-                let _ = PB.println(format!(
-                    "下载失败: {} - {}",
-                    resp.status(),
-                    resp.url()
-                ));
-                video_pbar.inc(1);
-                pb.finish_and_clear();
-                return;
-            }
-            if resp.status() == 206 {
-                resp.headers()
-                    .get(header::CONTENT_RANGE)
-                    .and_then(|v| v.to_str().ok())
-                    .and_then(|s| s.split('/').next_back())
-                    .and_then(|n| n.parse().ok())
-                    .unwrap_or(downloaded + resp.content_length().unwrap_or(0))
-            } else {
-                resp.content_length().unwrap_or(0)
-            };
+    if resp.status() == 416 {
+        let _ = PB.println(format!(
+            "文件已下载完成: {} - {}",
+            resp.status(),
+            resp.url()
+        ));
+        pb.finish_and_clear();
+        return Ok(());
+    }
+    if !(resp.status().is_success() || resp.status() == 206) {
+        let _ = PB.println(format!(
+            "Download failed: {} - {}",
+            resp.status(),
+            resp.url()
+        ));
+        pb.finish_and_clear();
+        return Err(anyhow::anyhow!(
+            "Download failed: {} - {}",
+            resp.status(),
+            resp.url()
+        ));
+    }
+    if resp.status() == 206 {
+        resp.headers()
+            .get(header::CONTENT_RANGE)
+            .and_then(|v| v.to_str().ok())
+            .and_then(|s| s.split('/').next_back())
+            .and_then(|n| n.parse().ok())
+            .unwrap_or(downloaded + resp.content_length().unwrap_or(0))
+    } else {
+        resp.content_length().unwrap_or(0)
+    };
 
-            let mut file = OpenOptions::new()
-                .write(true)
-                .create(true).truncate(true)
-                .open(&path)
-                .await.unwrap();
-            let mut pos = downloaded;
-            pb.set_length(pos + resp.content_length().unwrap_or(0));
-            let mut resp_stream = resp.bytes_stream();
-            while let Some(chunk) = resp_stream.next().await {
-                match chunk {
-                    Ok(chunk) => {
-                        let len = chunk.len();
-                        pos += len as u64;
-                        file.write_all(&chunk).await.unwrap();
-                        pb.set_position(pos);
-                    }
-                    Err(err) => {
-                        let _ = PB.println(format!("下载失败: {} - {}", att.name, err));
-                        return;
-                    }
-                }
+    let mut file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open(&path)
+        .await
+        .unwrap();
+    let mut pos = downloaded;
+    pb.set_length(pos + resp.content_length().unwrap_or(0));
+    let mut resp_stream = resp.bytes_stream();
+    while let Some(chunk) = resp_stream.next().await {
+        match chunk {
+            Ok(chunk) => {
+                let len = chunk.len();
+                pos += len as u64;
+                file.write_all(&chunk).await.unwrap();
+                pb.set_position(pos);
             }
-
-            file.flush().await.unwrap();
-
-            video_pbar.inc(1);
-            pb.finish_and_clear();
-        });
+            Err(err) => {
+                let _ = PB.println(format!("Download failed: {} - {}", att.name, err));
+                return Err(anyhow::anyhow!("Download failed: {} - {}", att.name, err));
+            }
+        }
     }
 
-    tasks.join_all().await;
+    file.flush().await.unwrap();
+
+    pb.finish_and_clear();
 
     Ok(())
 }
